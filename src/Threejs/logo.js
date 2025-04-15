67df12407aea83a65a6fa7eface916826c5467ab
Francis <fantobre@yahoo.com>
2025-04-15 12:00:47 -0600

    Update logo source to placeholder and add LICENSE file

--- src/Threejs/logo.js
+++ src/Threejs/logo.js
@@ -1,15 +1,14 @@
 import Image from "next/image";
-import logoFile from "../../public/aurum.png";
+import logoFile from "https://placehold.co/233x239/000/fff?text=Logo+Placeholder";
 
-export default function Logo () {
+export default function Logo() {
   return (
     <Image
-    src={logoFile}
-    alt="logo of provider"
-    width={233}
-    height={239}
-    className="absolute bottom-4 right-10"
-  />
-  
+      src={logoFile}
+      alt="logo of provider"
+      width={233}
+      height={239}
+      className="absolute bottom-4 right-10"
+    />
   );
 }
